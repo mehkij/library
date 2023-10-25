b6849b4a60f50c513f5e25ca8af04894f96992b0
Mehki Joseph <109314751+mehkij@users.noreply.github.com>
2023-10-25 08:32:53 -0400

    Add basic functionality to library

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
+const myLibrary = [];
+
 function Book(name, author, pages, status) {
-  this.name = name;
-  this.author = author;
-  this.pages = pages;
-  this.status = status;
+  this.name = name
+  this.author = author
+  this.pages = pages
+  this.status = status
   this.info = function() {
     return name + " by " + author + ", " + pages + ", " + status + ".";
   }
 }
 
+function addBookToLibrary() {
+  let input = prompt("Enter the name of the book you would like to add:");
+  myLibrary.push(input);
+}
